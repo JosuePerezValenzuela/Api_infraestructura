71edcb912fae767349df444fed9f2bf675c6fd9f
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-09-17 15:40:54 -0400

    Creacion del JOI

---
+++ src/config/validation.ts
@@ -0,0 +1,18 @@
+import * as Joi from 'joi';
+
+export const envSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+  PORT: Joi.number().default(3000),
+
+  // Base de datos
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432),
+  DB_NAME: Joi.string().required(),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+
+  // JWT
+  GLOBAL_PREFIX: Joi.string().default('api'),
+});
