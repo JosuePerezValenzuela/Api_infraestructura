a1a6546689ca92c2e5122391227eaf3164c8cfe1
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-09-18 09:17:22 -0400

    Validacion de las variables de entorno con JOI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './config/validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      //Valida el esquam del joi
+      validationSchema: envSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
