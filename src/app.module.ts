ff2ebfa81363601c06505db700ed07a3a0dddfda
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-09-18 10:11:03 -0400

    Coneccion a la BD y prueba

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { envSchema } from './config/validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -10,6 +11,23 @@ import { envSchema } from './config/validation';
       isGlobal: true,
       //Valida el esquam del joi
       validationSchema: envSchema,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) => ({
+        type: 'postgres',
+        host: cfg.get<string>('DB_HOST'),
+        port: cfg.get<number>('DB_PORT'),
+        database: cfg.get<string>('DB_NAME'),
+        username: cfg.get<string>('DB_USER'),
+        password: cfg.get<string>('DB_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: false,
+        schema: 'infraestructura',
+        logging: true,
+        extra: { application_name: 'Infra_nest' },
+      }),
     }),
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable, ServiceUnavailableException } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly dataSource: DataSource) {}
+
+  @Get('db')
+  async db() {
+    try {
+      await this.dataSource.query('SELECT 1');
+      return { status: 'up' };
+    } catch (err) {
+      throw new ServiceUnavailableException('Database is unreacheable');
+    }
   }
 }
