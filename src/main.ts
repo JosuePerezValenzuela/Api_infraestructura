4711b01483bad782dfcade20aa3ccef2e2a631a1
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-09-18 09:23:30 -0400

    Seguridad con helmet y uso del prefijo globla del .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cfg = app.get(ConfigService);
+  //Seguridad HTTP
+  app.use(helmet());
+  app.enableCors({ origin: true, credentials: true });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
