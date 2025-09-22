7cdb43a50e353d4256924ecd2a68b524a4217793
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-09-22 10:20:14 -0400

    creacion del entity

---
+++ src/modules/campus/infrastructure/campus.orm-entity.ts
@@ -0,0 +1,34 @@
+/* eslint-disable indent */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ schema: 'infraestructura', name: 'campus' })
+export class CampusOrmEntity {
+  @PrimaryGeneratedColumn({ type: 'integer' })
+  id!: number;
+
+  @Column({ type: 'varchar', length: 128 })
+  nombre!: string;
+
+  @Column({ type: 'varchar', length: 256 })
+  direccion!: string;
+
+  @Column({ type: 'point' })
+  coordenadas!: string;
+
+  @Column({ type: 'boolean', default: true })
+  activo!: boolean;
+
+  @Column({
+    type: 'timestamptz',
+    name: 'creado_en',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  creadoEn!: Date;
+
+  @Column({
+    type: 'timestamptz',
+    name: 'actualizado_en',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  actualizadoEn!: Date;
+}
