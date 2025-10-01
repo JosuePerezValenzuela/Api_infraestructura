7eb03d368058f5591435bd7dc3d799c85a8d66ae
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-01 11:48:11 -0400

    Se aniadio el puerto para edicion

--- src/modules/campus/domain/campus.repository.port.ts
+++ src/modules/campus/domain/campus.repository.port.ts
@@ -1,20 +1,3 @@
-export const CampusRepositoryPort = Symbol('CampusRepositoryPort');
-export interface CampusRepositoryPort {
-  create(input: {
-    nombre: string;
-    codigo: string;
-    direccion: string;
-    lat: number;
-    lng: number;
-  }): Promise<{ id: number }>;
-
-  list(opts: ListOptions): Promise<{
-    items: CampusListItem[];
-    total: number;
-  }>;
-}
-//Los dominios definen lo que necesita un 'puerto'
-
 export type OrderDirection = 'asc' | 'desc';
 export interface ListOptions {
   skip: number;
@@ -35,3 +18,31 @@ export interface CampusListItem {
   creado_en: Date;
   actualizado_en: Date;
 }
+
+export interface UpdateCampusInput {
+  codigo?: string;
+  nombre?: string;
+  direccion?: string;
+  lat?: number;
+  lng?: number;
+  activo?: boolean;
+}
+
+export const CampusRepositoryPort = Symbol('CampusRepositoryPort');
+export interface CampusRepositoryPort {
+  create(input: {
+    nombre: string;
+    codigo: string;
+    direccion: string;
+    lat: number;
+    lng: number;
+  }): Promise<{ id: number }>;
+
+  list(opts: ListOptions): Promise<{
+    items: CampusListItem[];
+    total: number;
+  }>;
+
+  update(id: number, input: UpdateCampusInput): Promise<boolean>;
+}
+//Los dominios definen lo que necesita un 'puerto'
