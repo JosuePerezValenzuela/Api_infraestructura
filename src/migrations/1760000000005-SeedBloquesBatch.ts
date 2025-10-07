a8ec111c56f4cf13faa62b68eed34cf197c4650d
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-07 09:07:43 -0400

    Seeder de bloques y cambio de atributos para tipo de bloques

--- src/migrations/1760000000005-SeedBloquesBatch.ts
+++ src/migrations/1760000000005-SeedBloquesBatch.ts
@@ -3,6 +3,21 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class SeedBloquesBatch1760000000005 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     // TODO: Add raw SQL inserts for bloques batch seeding.
+    await queryRunner.startTransaction();
+    try {
+      await queryRunner.query(
+        `
+          INSERT INTO infraestructura.bloques (nombre, nombre_corto, codigo, pisos, facultad_id, tipo_bloque_id)
+          VALUES
+            ("Edificio Nuevo de aulas", "Edifico nuevo", "123456789", 3, 1, 3)
+            ("Edifico de laboratorios", "Edificio labos", "987654321", 4, 1, 2)
+            ("Pasillo de gallineros", "Edificio de aulas", "111112222", 1, 2, 3)
+        `,
+      );
+    } catch (error) {
+      queryRunner.rollbackTransaction();
+      throw error;
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
