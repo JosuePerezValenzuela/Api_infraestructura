58543b2c1b3e2e6536aa262a447e495c22b02ae2
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-07 09:24:29 -0400

    Arreglo seeder de bloques y modelo de BD

--- src/migrations/1760000000005-SeedBloquesBatch.ts
+++ src/migrations/1760000000005-SeedBloquesBatch.ts
@@ -7,11 +7,11 @@ export class SeedBloquesBatch1760000000005 implements MigrationInterface {
     try {
       await queryRunner.query(
         `
-          INSERT INTO infraestructura.bloques (nombre, nombre_corto, codigo, pisos, facultad_id, tipo_bloque_id)
+          INSERT INTO infraestructura.bloques (nombre, nombre_corto, codigo, pisos, coordenadas, facultad_id, tipo_bloque_id)
           VALUES
-            ("Edificio Nuevo de aulas", "Edifico nuevo", "123456789", 3, 1, 3)
-            ("Edifico de laboratorios", "Edificio labos", "987654321", 4, 1, 2)
-            ("Pasillo de gallineros", "Edificio de aulas", "111112222", 1, 2, 3)
+            ("Edificio Nuevo de aulas", "Edifico nuevo", "123456789", 3, (-66,-18), 1, 3)
+            ("Edifico de laboratorios", "Edificio labos", "987654321", 4, (-66,-19), 1, 2)
+            ("Pasillo de gallineros", "Edificio de aulas", "111112222", 1, (-62,-18), 2, 3)
         `,
       );
     } catch (error) {
