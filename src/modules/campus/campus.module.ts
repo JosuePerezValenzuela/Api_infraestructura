516edfc95567dded20f969a6c98c64f0e9d0ef9b
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-13 15:28:01 -0400

    FEAT: Se anadio el modulo para la edicion del campo activo en cadena

--- src/modules/campus/campus.module.ts
+++ src/modules/campus/campus.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CampusOrmEntity } from './infrastructure/campus.orm-entity';
-import { CampusRepositoryPort } from './domain/campus.repository.port';
-import { CreateCampusUseCase } from './application/create-campus.usecase';
-import { CampusController } from './interface/campus.controller';
 import { TypeormCampusRepository } from './infrastructure/campus.typeorm.repository';
+import { CampusOrmEntity } from './infrastructure/campus.orm-entity';
+
+import { CampusRepositoryPort } from './domain/campus.repository.port';
+
+import { CreateCampusUseCase } from './application/create-campus.usecase';
 import { ListCampusUseCase } from './application/list-campus.usecase';
 import { UpdateCampusUseCase } from './application/update-campus.usecase';
+
+import { CampusController } from './interface/campus.controller';
+
+import { RelationshipsModule } from '../_shared/relationships/relationships.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([CampusOrmEntity])],
+  imports: [TypeOrmModule.forFeature([CampusOrmEntity]), RelationshipsModule],
   controllers: [CampusController],
   providers: [
     CreateCampusUseCase,
