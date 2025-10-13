d7da7e74833182d133232190181aa4334fdb9e4a
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-13 15:29:07 -0400

    FEAT: Anadio el modulo de edicion del campo inactivo en cadena al modulo de la facultad

--- src/modules/facultad/facultad.module.ts
+++ src/modules/facultad/facultad.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CampusModule } from '../campus/campus.module';
+import { RelationshipsModule } from '../_shared/relationships/relationships.module';
+
 import { CreateFacultadUseCase } from './application/create-facultad.usecase';
 import { ListFacultadesUseCase } from './application/list-facultades.usecase';
+
 import { FacultadRepositoryPort } from './domain/facultad.repository.port';
+
 import { TypeormFacultadRepository } from './infraestructure/persistence/facultad.typeorm.repository';
+
 import { FacultadController } from './interface/facultad.controller';
 
 @Module({
-  imports: [CampusModule],
+  imports: [CampusModule, RelationshipsModule],
   controllers: [FacultadController],
   providers: [
     CreateFacultadUseCase,
