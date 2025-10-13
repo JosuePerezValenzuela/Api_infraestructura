05b598fe8c99123319e635311289825178444fcf
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-13 08:45:26 -0400

    FEAT: modificacion del module de facultades para completar la implementacion

--- src/modules/facultad/facultad.module.ts
+++ src/modules/facultad/facultad.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CampusModule } from '../campus/campus.module';
 import { CreateFacultadUseCase } from './application/create-facultad.usecase';
+import { ListFacultadesUseCase } from './application/list-facultades.usecase';
 import { FacultadRepositoryPort } from './domain/facultad.repository.port';
 import { TypeormFacultadRepository } from './infraestructure/persistence/facultad.typeorm.repository';
 import { FacultadController } from './interface/facultad.controller';
@@ -10,11 +11,12 @@ import { FacultadController } from './interface/facultad.controller';
   controllers: [FacultadController],
   providers: [
     CreateFacultadUseCase,
+    ListFacultadesUseCase,
     {
       provide: FacultadRepositoryPort,
       useClass: TypeormFacultadRepository,
     },
   ],
-  exports: [CreateFacultadUseCase],
+  exports: [CreateFacultadUseCase, ListFacultadesUseCase],
 })
 export class FacultadModule {}
