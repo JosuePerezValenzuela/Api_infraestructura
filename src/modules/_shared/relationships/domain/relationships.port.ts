6c9e9de9dc583417cd9e9e9d967268a1c379f105
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-13 15:16:35 -0400

    FEAT: Creacion del puerto para las inactivaciones en cascada

---
+++ src/modules/_shared/relationships/domain/relationships.port.ts
@@ -0,0 +1,12 @@
+export const RelationshipsPort = Symbol('RelationshipsPort');
+
+export interface RelationshipsPort {
+  //Marca como inactivo todas las entidades descendientes de un campus
+  markCampusCascadeIncative(campusId: number): Promise<void>;
+
+  markFacultadCascadeInactive(facultadId: number): Promise<void>;
+
+  markBloquesCascadeInactive(bloqueId: number): Promise<void>;
+
+  markAmbientesCascadeInactive(ambientId: number): Promise<void>;
+}
