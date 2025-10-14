a2c817bb328390e7a2b0c87147014ea486025bb0
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-14 14:13:10 -0400

    FEAT: ELiminacion del metodo ya que toda la logica esta en el puerto compartido que permite eliminar en cascada

--- src/modules/campus/domain/campus.repository.port.ts
+++ src/modules/campus/domain/campus.repository.port.ts
@@ -48,7 +48,5 @@ export interface CampusRepositoryPort {
   isCodeTaken(codigo: string, excludeId?: number): Promise<boolean>;
 
   update(id: number, input: UpdateCampusInput): Promise<{ id: number }>;
-
-  deleteById(id: number): Promise<{ id: number } | null>;
 }
 //Los dominios definen lo que necesita un 'puerto'
