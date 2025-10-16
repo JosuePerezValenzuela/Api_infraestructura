0bff8d7d92464ae42a4d24a6fc313f52f0538cfd
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-16 09:24:33 -0400

    FEAT: Creacion de un metodo que obtiene una facultad por su id

--- src/modules/facultad/domain/facultad.repository.port.ts
+++ src/modules/facultad/domain/facultad.repository.port.ts
@@ -4,6 +4,7 @@ import type {
   ListFacultadesQuery,
   ListFacultadesResult,
   UpdateFacultadesInput,
+  ListFacultadesItem,
 } from './facultad.list.types';
 
 // Tipo que espera el metodo create
@@ -21,6 +22,8 @@ export interface FacultadRepositoryPort {
   create(data: CreateFacultadData): Promise<{ id: number }>;
 
   isCodeTaken(codigo: string): Promise<boolean>;
+
+  findById(id: number): Promise<ListFacultadesItem | null>;
 
   findPaginated(opts: ListFacultadesQuery): Promise<ListFacultadesResult>;
 
