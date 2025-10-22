ce4a688a2f0222a6276ab404ca69afa0d219d95c
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-22 10:04:54 -0400

    FIX: Arreglo de typo

--- src/modules/tipo-bloque/domain/tipo-bloque.repository.port.ts
+++ src/modules/tipo-bloque/domain/tipo-bloque.repository.port.ts
@@ -3,7 +3,7 @@ import { CreateTipoBloqueCommand } from './commands/create-tipo-bloque.command';
 export const TipoBloqueRepositoryPort = Symbol('TipoBloqueRepositoryPort');
 
 export interface TipoBloqueRepositoryPort {
-  create(commnad: CreateTipoBloqueCommand): Promise<{ id: number }>;
+  create(command: CreateTipoBloqueCommand): Promise<{ id: number }>;
 
   isNameTaken(nombre: string): Promise<boolean>;
 }
