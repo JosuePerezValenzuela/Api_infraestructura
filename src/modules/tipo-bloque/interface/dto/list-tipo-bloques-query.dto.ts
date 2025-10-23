c5f7c9bba06606e61a46e0d10b45556728bc2cf8
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-23 11:18:20 -0400

    FEAT: impleentacion del dto

---
+++ src/modules/tipo-bloque/interface/dto/list-tipo-bloques-query.dto.ts
@@ -0,0 +1,44 @@
+/* eslint-disable indent */
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsIn,
+  IsInt,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
+export class ListTipoBloquesQueryDto {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt({ message: 'La pagina debe ser un numero' })
+  @Min(1, { message: 'La pagina debe ser un numero mayor o igual a 1' })
+  page?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt({ message: 'El limite debe ser un numero' })
+  @Min(1, { message: 'El limite debe ser un numero mayor o igual a 1' })
+  limit?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: 'El search debe ser una cadena' })
+  @MaxLength(64, { message: 'La busqueda no puede ser mayor a 64 caracteres' })
+  search?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsIn(['nombre', 'descripcion', 'creado_en'], {
+    message: 'Solo se puede ordenar por nombre, descripcion o creado_en',
+  })
+  orderBy?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsIn(['asc', 'desc'], {
+    message: 'La direccion de orden debe ser asc o desc',
+  })
+  orderDir?: string;
+}
