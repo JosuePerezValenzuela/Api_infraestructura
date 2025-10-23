2040729f5013f8c792859b3fac2af199bac03e21
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-23 11:35:40 -0400

    FEAT: Se anadio el usecase al module

--- src/modules/tipo-bloque/tipo-bloque.module.ts
+++ src/modules/tipo-bloque/tipo-bloque.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TipoBloqueController } from './interface/tipo-bloque.controller';
 import { CreateTipoBloqueUseCase } from './application/create-tipo-bloque.usecase';
+import { ListTipoBloquesUseCase } from './application/list-tipo-bloques.usecase';
 import { TipoBloqueRepositoryPort } from './domain/tipo-bloque.repository.port';
 import { TypeormTipoBloqueRepository } from './infrastructure/persistence/typeorm-tipo-bloque.repository';
 
@@ -8,11 +9,12 @@ import { TypeormTipoBloqueRepository } from './infrastructure/persistence/typeor
   controllers: [TipoBloqueController],
   providers: [
     CreateTipoBloqueUseCase,
+    ListTipoBloquesUseCase,
     {
       provide: TipoBloqueRepositoryPort,
       useClass: TypeormTipoBloqueRepository,
     },
   ],
-  exports: [CreateTipoBloqueUseCase],
+  exports: [CreateTipoBloqueUseCase, ListTipoBloquesUseCase],
 })
 export class TipoBloqueModule {}
