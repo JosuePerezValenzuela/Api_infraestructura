6bbbd7ccb3ed93165ebc3cd15d0e38a66ebcc458
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-10-29 09:33:35 -0400

    FEAT: Implementacion del modulo

--- src/modules/tipo-bloque/tipo-bloque.module.ts
+++ src/modules/tipo-bloque/tipo-bloque.module.ts
@@ -3,15 +3,19 @@ import { TipoBloqueController } from './interface/tipo-bloque.controller';
 import { CreateTipoBloqueUseCase } from './application/create-tipo-bloque.usecase';
 import { ListTipoBloquesUseCase } from './application/list-tipo-bloques.usecase';
 import { UpdateTipoBloqueUseCase } from './application/update-tipo-bloque.usecase';
+import { DeleteTipoBloqueUseCase } from './application/delete-tipo-bloque.usecase';
 import { TipoBloqueRepositoryPort } from './domain/tipo-bloque.repository.port';
+import { RelationshipsModule } from '../_shared/relationships/relationships.module';
 import { TypeormTipoBloqueRepository } from './infrastructure/persistence/typeorm-tipo-bloque.repository';
 
 @Module({
+  imports: [RelationshipsModule],
   controllers: [TipoBloqueController],
   providers: [
     CreateTipoBloqueUseCase,
     ListTipoBloquesUseCase,
     UpdateTipoBloqueUseCase,
+    DeleteTipoBloqueUseCase,
     {
       provide: TipoBloqueRepositoryPort,
       useClass: TypeormTipoBloqueRepository,
@@ -21,6 +25,7 @@ import { TypeormTipoBloqueRepository } from './infrastructure/persistence/typeor
     CreateTipoBloqueUseCase,
     ListTipoBloquesUseCase,
     UpdateTipoBloqueUseCase,
+    DeleteTipoBloqueUseCase,
   ],
 })
 export class TipoBloqueModule {}
