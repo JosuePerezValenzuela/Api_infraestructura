4dfcfbcf6801dcf60b50b19bc8a6cfc8b29dea0b
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-11-04 09:01:13 -0400

    FEAT: Creacion del puerto

---
+++ src/modules/bloque/domain/bloque.repository.port.ts
@@ -0,0 +1,9 @@
+import { CreateBloqueCommand } from './commands/create-bloque.command';
+
+export const BloqueRepositoryPort = Symbol('BloqueRepositoryPort');
+
+export interface BloqueRepositoryPort {
+  create(commmand: CreateBloqueCommand): Promise<number>;
+
+  isCodeTaken(codigo: string): Promise<boolean>;
+}
