8cded75b67f2cb97932cafd4fbbd000ed61d7d73
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-11-04 08:57:30 -0400

    FEAT: Command para crear un bloque

---
+++ src/modules/bloque/domain/commands/create-bloque.command.ts
@@ -0,0 +1,12 @@
+// Los datos minimos que necesita la capa de infraestructura para crear un bloque
+
+export interface CreateBloqueCommand {
+  codigo: string;
+  nombre: string;
+  nombre_corto: string;
+  pointLiteral: string;
+  pisos: number;
+  activo: boolean;
+  facultad_id: number;
+  tipo_bloque_id: number;
+}
