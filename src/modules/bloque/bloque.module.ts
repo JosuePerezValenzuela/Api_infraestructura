8c2a2482990b726bd4e1a8e1f1d0783fb954a16c
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-11-06 11:10:15 -0400

    FEAT: Agregaciones al modulo

--- src/modules/bloque/bloque.module.ts
+++ src/modules/bloque/bloque.module.ts
@@ -1,25 +1,28 @@
 import { Module } from '@nestjs/common';
 import { FacultadModule } from '../facultad/facultad.module';
 import { TipoBloqueModule } from '../tipo-bloque/tipo-bloque.module';
+import { RelationshipsModule } from '../_shared/relationships/relationships.module';
 
 import { BloqueController } from './interface/bloque.controller';
 import { CreateBloqueUseCase } from './application/create-bloque.usecase';
 import { ListBloquesUseCase } from './application/list-bloques.usecase';
+import { UpdateBloqueUseCase } from './application/update-bloque.usecase';
 
 import { BloqueRepositoryPort } from './domain/bloque.repository.port';
 import { TypeormBloqueRepository } from './infrastructure/persistence/typeorm-bloque.repository';
 
 @Module({
-  imports: [FacultadModule, TipoBloqueModule],
+  imports: [FacultadModule, TipoBloqueModule, RelationshipsModule],
   controllers: [BloqueController],
   providers: [
     CreateBloqueUseCase,
     ListBloquesUseCase,
+    UpdateBloqueUseCase,
     {
       provide: BloqueRepositoryPort,
       useClass: TypeormBloqueRepository,
     },
   ],
-  exports: [BloqueRepositoryPort, ListBloquesUseCase],
+  exports: [BloqueRepositoryPort, ListBloquesUseCase, UpdateBloqueUseCase],
 })
 export class BloqueModule {}
