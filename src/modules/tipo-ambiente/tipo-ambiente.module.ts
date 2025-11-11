dbe16178d064d9045aa190e2e0776084673a4a3c
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-11-11 10:26:27 -0400

    FEAT: Expansion del module

--- src/modules/tipo-ambiente/tipo-ambiente.module.ts
+++ src/modules/tipo-ambiente/tipo-ambiente.module.ts
@@ -4,12 +4,14 @@ import { CreateTipoAmbienteUseCase } from './application/create-tipo-ambiente.us
 import { TipoAmbienteController } from './interface/tipo-ambiente.controller';
 import { TipoAmbienteRepositoryPort } from './domain/tipo-ambiente.repository.port';
 import { TypeormTipoAmbienteRepository } from './infrastructure/persistence/typeorm-tipo-ambiente.repository';
+import { ListTipoAmbientesUseCase } from './application/list-tipo-ambientes.usecase';
 
 @Module({
   imports: [TypeOrmModule.forFeature([])],
   controllers: [TipoAmbienteController],
   providers: [
     CreateTipoAmbienteUseCase,
+    ListTipoAmbientesUseCase,
     {
       provide: TipoAmbienteRepositoryPort,
       useClass: TypeormTipoAmbienteRepository,
