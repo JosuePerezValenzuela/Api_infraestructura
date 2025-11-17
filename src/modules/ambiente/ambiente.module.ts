5f957e060988d0635deccf1adffe961277186ea0
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-11-17 11:36:48 -0400

    FEAT: Incorporacion del nuevo caso de uso al modulo

--- src/modules/ambiente/ambiente.module.ts
+++ src/modules/ambiente/ambiente.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AmbienteController } from './interface/ambiente.controller';
 import { CreateAmbienteUseCase } from './application/create-ambiente.usecase';
+import { ListAmbientesUseCase } from './application/list-ambientes.usecase';
 import { AmbienteRepositoryPort } from './domain/ambiente.repository.port';
 import { TypeormAmbienteRepository } from './infrastructure/persistence/typeorm-ambiente.repository';
 import { BloqueModule } from '../bloque/bloque.module';
@@ -11,11 +12,16 @@ import { TipoAmbienteModule } from '../tipo-ambiente/tipo-ambiente.module';
   controllers: [AmbienteController],
   providers: [
     CreateAmbienteUseCase,
+    ListAmbientesUseCase,
     {
       provide: AmbienteRepositoryPort,
       useClass: TypeormAmbienteRepository,
     },
   ],
-  exports: [AmbienteRepositoryPort, CreateAmbienteUseCase],
+  exports: [
+    AmbienteRepositoryPort,
+    CreateAmbienteUseCase,
+    ListAmbientesUseCase,
+  ],
 })
 export class AmbienteModule {}
