e39e12157b3b3739d50b1db679b39ad4a81feedf
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-11-24 13:55:45 -0400

    FEAT: Ampliacion del modulo para editar un ambiente

--- src/modules/activo/activo.module.ts
+++ src/modules/activo/activo.module.ts
@@ -3,6 +3,7 @@ import { ActivoController } from './interface/activo.controller';
 import { ListActivosUseCase } from './application/list-activos.usecase';
 import { CreateActivoUseCase } from './application/create-activo.usecase';
 import { DeleteActivoUseCase } from './application/delete-activo.usecase';
+import { UpdateActivoUseCase } from './application/update-activo.usecase';
 import { ActivoRepositoryPort } from './domain/activo.repository.port';
 import { TypeormActivoRepository } from './infrastructure/persistence/typeorm-activo.repository';
 
@@ -12,6 +13,7 @@ import { TypeormActivoRepository } from './infrastructure/persistence/typeorm-ac
     ListActivosUseCase,
     CreateActivoUseCase,
     DeleteActivoUseCase,
+    UpdateActivoUseCase,
     {
       provide: ActivoRepositoryPort,
       useClass: TypeormActivoRepository,
@@ -22,6 +24,7 @@ import { TypeormActivoRepository } from './infrastructure/persistence/typeorm-ac
     ListActivosUseCase,
     CreateActivoUseCase,
     DeleteActivoUseCase,
+    UpdateActivoUseCase,
   ],
 })
 export class ActivoModule {}
