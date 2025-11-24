c96f13f511307f6e15f6a8e39d80e2d97ac8994f
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-11-24 11:38:55 -0400

    FEAT: Ampliacion del puerto para crear activos

--- src/modules/activo/domain/activo.repository.port.ts
+++ src/modules/activo/domain/activo.repository.port.ts
@@ -1,7 +1,12 @@
+import { CreateActivoCommand } from './commands/create-activo.command';
 import { ListActivosOptions, ListActivosResult } from './activo.list.types';
 
 export const ActivoRepositoryPort = Symbol('ActivoRepositoryPort');
 
 export interface ActivoRepositoryPort {
+  create(command: CreateActivoCommand): Promise<{ id: number }>;
+
+  isNiaTaken(nia: string): Promise<boolean>;
+
   list(options: ListActivosOptions): Promise<ListActivosResult>;
 }
