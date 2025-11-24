527ab9954b34092b82b34dcebe198a98b545f4cd
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-11-24 13:51:04 -0400

    FEAT: Ampliacion del puerto a nuevos metodos

--- src/modules/activo/domain/activo.repository.port.ts
+++ src/modules/activo/domain/activo.repository.port.ts
@@ -1,5 +1,6 @@
 import { CreateActivoCommand } from './commands/create-activo.command';
 import { DeleteActivoCommand } from './commands/delete-activo.command';
+import { UpdateActivoCommand } from './commands/update-activo.command';
 import { ListActivosOptions, ListActivosResult } from './activo.list.types';
 
 export const ActivoRepositoryPort = Symbol('ActivoRepositoryPort');
@@ -7,11 +8,13 @@ export const ActivoRepositoryPort = Symbol('ActivoRepositoryPort');
 export interface ActivoRepositoryPort {
   create(command: CreateActivoCommand): Promise<{ id: number }>;
 
-  isNiaTaken(nia: string): Promise<boolean>;
+  isNiaTaken(nia: string, excludeId?: number): Promise<boolean>;
 
   findById(id: number): Promise<{ id: number } | null>;
 
   delete(command: DeleteActivoCommand): Promise<{ id: number }>;
+
+  update(command: UpdateActivoCommand): Promise<{ id: number }>;
 
   list(options: ListActivosOptions): Promise<ListActivosResult>;
 }
