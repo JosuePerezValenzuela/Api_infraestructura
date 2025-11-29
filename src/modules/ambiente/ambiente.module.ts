734d42c62ecb4d76dff0c0b9aa16ecdc7b7f6139
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2025-11-28 21:26:09 -0400

    FEAT: Ampliacion del modulo

--- src/modules/ambiente/ambiente.module.ts
+++ src/modules/ambiente/ambiente.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AmbienteController } from './interface/ambiente.controller';
 import { CreateAmbienteUseCase } from './application/create-ambiente.usecase';
 import { ListAmbientesUseCase } from './application/list-ambientes.usecase';
+import { ListAmbientesDisponiblesUseCase } from './application/list-ambientes-disponibles.usecase';
 import { DeleteAmbienteUseCase } from './application/delete-ambiente.usecase';
 import { UpdateAmbienteUseCase } from './application/update-ambiente.usecase';
 import { ReplaceHorariosUseCase } from './application/replace-horarios.usecase';
 import { AmbienteRepositoryPort } from './domain/ambiente.repository.port';
 import { HorarioRepositoryPort } from './domain/horario.repository.port';
+import { AmbientesDisponiblesRepositoryPort } from './domain/ambiente.disponibles.port';
 import { TypeormAmbienteRepository } from './infrastructure/persistence/typeorm-ambiente.repository';
+import { TypeormAmbientesDisponiblesRepository } from './infrastructure/persistence/typeorm-ambientes-disponibles.repository';
 import { TypeormHorarioRepository } from './infrastructure/persistence/typeorm-horario.repository';
 import { BloqueModule } from '../bloque/bloque.module';
 import { TipoAmbienteModule } from '../tipo-ambiente/tipo-ambiente.module';
@@ -18,12 +21,17 @@ import { TipoAmbienteModule } from '../tipo-ambiente/tipo-ambiente.module';
   providers: [
     CreateAmbienteUseCase,
     ListAmbientesUseCase,
+    ListAmbientesDisponiblesUseCase,
     DeleteAmbienteUseCase,
     UpdateAmbienteUseCase,
     ReplaceHorariosUseCase,
     {
       provide: AmbienteRepositoryPort,
       useClass: TypeormAmbienteRepository,
+    },
+    {
+      provide: AmbientesDisponiblesRepositoryPort,
+      useClass: TypeormAmbientesDisponiblesRepository,
     },
     {
       provide: HorarioRepositoryPort,
@@ -34,6 +42,7 @@ import { TipoAmbienteModule } from '../tipo-ambiente/tipo-ambiente.module';
     AmbienteRepositoryPort,
     CreateAmbienteUseCase,
     ListAmbientesUseCase,
+    ListAmbientesDisponiblesUseCase,
     DeleteAmbienteUseCase,
     UpdateAmbienteUseCase,
     ReplaceHorariosUseCase,
