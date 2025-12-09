ede8e177fe7368826cbf3ed47652f37efa3f180b
Josue Perez <josueperezv2004@gmail.com>
2025-12-09 15:32:32 -0400

    FEAT: Reporte auxiliar para el generador de reportes

---
+++ src/modules/reportes/domain/ports/inventario-reporte.repository.ts
@@ -0,0 +1,9 @@
+import { InventarioReporteViewModel } from '../models/inventario.view-model';
+
+export interface InventarioReporteRepository {
+  obtener_por_campus(campus_id: string): Promise<InventarioReporteViewModel>;
+  obtener_por_facultad(
+    facultad_id: string,
+  ): Promise<InventarioReporteViewModel>;
+  obtener_por_bloque(bloque_id: string): Promise<InventarioReporteViewModel>;
+}
