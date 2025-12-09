df16689556c8736c365291b1cc14458dd255adf9
Josue Perez <josueperezv2004@gmail.com>
2025-12-09 15:33:34 -0400

    FEAT: Contrato del DTO

--- src/modules/reportes/interface/dto/generar-reporte-inventario.dto.ts
+++ src/modules/reportes/interface/dto/generar-reporte-inventario.dto.ts
@@ -1,1 +1,29 @@
-export class GenerarReporteInventarioDto {}
+/* eslint-disable indent */
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export enum ReporteScope {
+  CAMPUS = 'campus',
+  FACULTAD = 'facultad',
+  BLOQUE = 'bloque',
+}
+
+export enum ReporteFormato {
+  XLSX = 'xlsx',
+  PDF = 'pdf',
+}
+
+export class GenerarReporteInventarioDto {
+  @IsEnum(ReporteScope)
+  scope: ReporteScope;
+
+  @IsString()
+  @IsNotEmpty()
+  scopeId: string;
+
+  @IsEnum(ReporteFormato)
+  formato: ReporteFormato;
+
+  @IsOptional()
+  @IsString()
+  locale?: string;
+}
