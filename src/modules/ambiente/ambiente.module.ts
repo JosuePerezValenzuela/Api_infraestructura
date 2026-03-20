a468eeffc9702ef598d1ebf87bbd1e1700f34e21
Josue Perez Valenzuela <josueperezv2004@gmail.com>
2026-03-20 10:02:34 -0400

    refactor(ambiente): remove HorarioRepositoryPort from module - no longer needed

--- src/modules/ambiente/ambiente.module.ts
+++ src/modules/ambiente/ambiente.module.ts
@@ -8,11 +8,9 @@ import { DeleteAmbienteUseCase } from './application/delete-ambiente.usecase';
 import { UpdateAmbienteUseCase } from './application/update-ambiente.usecase';
 import { ReplaceHorariosUseCase } from './application/replace-horarios.usecase';
 import { AmbienteRepositoryPort } from './domain/ambiente.repository.port';
-import { HorarioRepositoryPort } from './domain/horario.repository.port';
 import { AmbientesDisponiblesRepositoryPort } from './domain/ambiente.disponibles.port';
 import { TypeormAmbienteRepository } from './infrastructure/persistence/typeorm-ambiente.repository';
 import { TypeormAmbientesDisponiblesRepository } from './infrastructure/persistence/typeorm-ambientes-disponibles.repository';
-import { TypeormHorarioRepository } from './infrastructure/persistence/typeorm-horario.repository';
 import { BloqueModule } from '../bloque/bloque.module';
 import { TipoAmbienteModule } from '../tipo-ambiente/tipo-ambiente.module';
 
@@ -35,10 +33,6 @@ import { TipoAmbienteModule } from '../tipo-ambiente/tipo-ambiente.module';
       provide: AmbientesDisponiblesRepositoryPort,
       useClass: TypeormAmbientesDisponiblesRepository,
     },
-    {
-      provide: HorarioRepositoryPort,
-      useClass: TypeormHorarioRepository,
-    },
   ],
   exports: [
     AmbienteRepositoryPort,
@@ -49,7 +43,6 @@ import { TipoAmbienteModule } from '../tipo-ambiente/tipo-ambiente.module';
     DeleteAmbienteUseCase,
     UpdateAmbienteUseCase,
     ReplaceHorariosUseCase,
-    HorarioRepositoryPort,
   ],
 })
 export class AmbienteModule {}
